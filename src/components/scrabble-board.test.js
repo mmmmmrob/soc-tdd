5871f9d3153046fc37e2d6248543ea2f4bdc97b6
Rob Styles <rob@styles.to>
2020-03-19 13:35:43 +0000

    more todos for component

--- src/components/scrabble-board.test.js
+++ src/components/scrabble-board.test.js
@@ -3,11 +3,15 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import ScrabbleBoard from "./scrabble-board";
 
 describe("ScrabbleBoard Component", () => {
-  it("should render a table", () => {
+  it("should render as a table", () => {
     const { getByRole } = render(<ScrabbleBoard />);
     expect(getByRole("table")).toBeTruthy();
   });
 
+  it.todo("should render 15 rows");
+  it.todo("should render 15 cells within each row");
   it.todo("should render an empty board");
-  it.todo("should render a board with words on");
+  it.todo("should render a board with a word going across");
+  it.todo("should render a board with a word going down");
+  it.todo("should render a board with several words going across and down");
 });
