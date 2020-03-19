9b56248f521ab4ba8ec74db95cfdfafe73131f72
Rob Styles <rob@styles.to>
2020-03-19 16:05:39 +0000

    basic scrabble board

--- src/models/scrabble-board.js
+++ src/models/scrabble-board.js
@@ -1,1 +1,23 @@
-export class ScrabbleBoard {}
+export class ScrabbleBoard {
+  constructor() {
+    this._board = [...Array(15)].map(() => {
+      return Array(15).fill("-");
+    });
+  }
+
+  play(x, y, dir, word) {
+    word = [...word];
+    word.forEach(letter => {
+      if (this._board[y][x] !== "-" && this._board[y][x] !== letter)
+        throw new Error("Letters clash with existing words");
+      this._board[y][x] = letter;
+      if (dir === "across") x++;
+      else y++;
+    });
+    return this.toString();
+  }
+
+  toString() {
+    return this._board.map(row => row.join("")).join("\n");
+  }
+}
--- src/models/scrabble-board.test.js
+++ src/models/scrabble-board.test.js
@@ -2,18 +2,33 @@ import { ScrabbleBoard } from "./scrabble-board";
 import { testBoards } from "./test-boards";
 
 describe("Scrabble Board", () => {
-  it.skip("should return the board as a multiline string", () => {
+  it("should return the board as a multiline string", () => {
     const board = new ScrabbleBoard();
     expect(board.toString()).toEqual(testBoards.empty);
   });
 
-  it.skip("should store a word going left-to-right", () => {
+  it("should store a word going left-to-right", () => {
     const board = new ScrabbleBoard();
     const boardString = board.play(3, 5, "across", "banana");
     expect(boardString).toEqual(testBoards.banana);
   });
 
-  it.todo("should store a word going top-to-bottom");
-  it.todo("should store a word that correctly overlaps existing words");
-  it.todo("should throw for a word that incorrectly overlaps existing words");
+  it("should store a word going top-to-bottom", () => {
+    const board = new ScrabbleBoard();
+    const boardString = board.play(3, 5, "down", "apple");
+    expect(boardString).toEqual(testBoards.apple);
+  });
+
+  it("should store a word that correctly overlaps existing words", () => {
+    const board = new ScrabbleBoard();
+    board.play(5, 3, "across", "orange");
+    board.play(7, 2, "down", "tan");
+    expect(board.toString()).toEqual(testBoards.orange_tan);
+  });
+
+  it("should throw for a word that incorrectly overlaps existing words", () => {
+    const board = new ScrabbleBoard();
+    board.play(5, 3, "across", "orange");
+    expect(() => board.play(7, 2, "down", "ton")).toThrow();
+  });
 });
